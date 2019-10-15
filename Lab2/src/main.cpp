8f72981cf31d8476804b792b18c534fb99060363
Jake Barcelona <jacobcole117@gmail.com>
2019-10-15 14:34:14 -0500

    Bug fix

--- Lab2/src/main.cpp
+++ Lab2/src/main.cpp
@@ -12,7 +12,7 @@ int main(int argc,char*argv[])
     {
         s.Load("src/cmake-build-debug/largeGraph.txt","src/cmake-build-debug/largeWeights.txt","src/cmake-build-debug/largePositions.txt",i);
         //Loop through each algo
-        for(int a=4;a<6;a++)
+        for(int a=0;a<6;a++)
         {
             s.Select(a);
             int src=atoi(argv[1]);
